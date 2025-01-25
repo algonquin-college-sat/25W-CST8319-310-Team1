12741e3b754020255e98fbf335b657b17a2ce46a
gulnurkaztai <93098410+gulnurkaztai@users.noreply.github.com>
2025-01-25 15:14:03 -0500

    initial

---
+++ app/src/main/java/algonquin/cst2335/enigmaticocrapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package algonquin.cst2335.enigmaticocrapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
