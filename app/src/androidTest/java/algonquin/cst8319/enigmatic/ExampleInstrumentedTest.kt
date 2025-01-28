576bd7773fdc74bc2374695055bcf8b44fbfe814
phil0440 <phil0440@algonquinlive.com>
2025-01-28 13:47:33 -0500

    refactor: adjust project and package naming structure
    Modified project name to 'enigmatic_ocr'. Refactored app package directory hierarchy to 'algonquin.cst8319.enigmatic'. Changes are reflected in all gradle, xml and source code files.

--- app/src/androidTest/java/algonquin/cst8319/enigmatic/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/algonquin/cst8319/enigmatic/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package algonquin.cst2335.enigmaticocrapp
+package algonquin.cst8319.enigmatic
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
