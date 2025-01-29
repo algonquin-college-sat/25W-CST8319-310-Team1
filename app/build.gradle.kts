020ee9764ee8e0ec412e37fc3fa9c870dd541dfe
Naomi <bell0418@algonquinlive.com>
2025-01-29 18:41:03 -0500

    build: updated serialization implementation
    Updated serialization implementation to 1.7.0.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-    kotlin("plugin.serialization") version "1.9.22"
+    kotlin("plugin.serialization") version "2.0.0"
 }
 
 android {
